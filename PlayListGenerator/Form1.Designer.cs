6c16e04da6ec99c4ff93617ef273bd9ebde7296a
Кирилл Щербо <Daliys133@gmail.com>
2019-10-15 22:52:55 +0300

    Добавлен функция считывания из файла

--- PlayListGenerator/Form1.Designer.cs
+++ PlayListGenerator/Form1.Designer.cs
@@ -28,23 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelBackground = new System.Windows.Forms.Panel();
             this.panelMenu = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.panelBackground.SuspendLayout();
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.panelOne = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
+            this.panelBackground.SuspendLayout();
+            this.panelOne.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panelBackground
-            // 
-            this.panelBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panelBackground.Controls.Add(this.panelMenu);
-            this.panelBackground.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panelBackground.Location = new System.Drawing.Point(0, 0);
-            this.panelBackground.Margin = new System.Windows.Forms.Padding(0);
-            this.panelBackground.Name = "panelBackground";
-            this.panelBackground.Size = new System.Drawing.Size(886, 611);
-            this.panelBackground.TabIndex = 0;
             // 
             // panelMenu
             // 
@@ -59,6 +52,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
@@ -67,6 +61,50 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panelBackground
+            // 
+            this.panelBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelBackground.Controls.Add(this.panelOne);
+            this.panelBackground.Controls.Add(this.panelMenu);
+            this.panelBackground.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelBackground.Location = new System.Drawing.Point(0, 0);
+            this.panelBackground.Margin = new System.Windows.Forms.Padding(0);
+            this.panelBackground.Name = "panelBackground";
+            this.panelBackground.Size = new System.Drawing.Size(886, 611);
+            this.panelBackground.TabIndex = 0;
+            this.panelBackground.Paint += new System.Windows.Forms.PaintEventHandler(this.panelBackground_Paint);
+            // 
+            // panelOne
+            // 
+            this.panelOne.BackColor = System.Drawing.Color.Green;
+            this.panelOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelOne.Controls.Add(this.label1);
+            this.panelOne.Controls.Add(this.button2);
+            this.panelOne.Location = new System.Drawing.Point(75, 0);
+            this.panelOne.Margin = new System.Windows.Forms.Padding(0);
+            this.panelOne.Name = "panelOne";
+            this.panelOne.Size = new System.Drawing.Size(811, 611);
+            this.panelOne.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
@@ -83,17 +121,22 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panelBackground.ResumeLayout(false);
             this.panelMenu.ResumeLayout(false);
+            this.panelBackground.ResumeLayout(false);
+            this.panelOne.ResumeLayout(false);
+            this.panelOne.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panelBackground;
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.Panel panelOne;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PlayListGenerator/Form1.cs
+++ PlayListGenerator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace PlayListGenerator
 {
@@ -19,12 +20,40 @@ namespace PlayListGenerator
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void panelBackground_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        string path = @"C:\Users\User\Source\Repos\Daliys\PlayListGenerator\PlayListGenerator\Futuris.xlsx";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            
+            //label1.Text = "Row " + xlRange.Rows.Count + " Col " + xlRange.Columns.Count;
+            string res = "";
+            for (int i = 8; i < 48; i++)
+            {
+                for (int j = 2; j < 63; j++)
+                {
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        res += xlRange.Cells[i, j].Value2.ToString() + " ";
+                    else res += " ";
+                }
+                res += "\n";
+            }
+            label1.Text = res;
         }
     }
 }
